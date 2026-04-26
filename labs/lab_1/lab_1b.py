bfc8e1a4384d69dee55d226df3594ca7946839c4
raghav-pillai <raghav.pillai@outlook.com>
2026-04-25 19:53:41 -0500

    Added sanitization to prevent errors

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -36,14 +36,24 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
             raise ValueError("Cannot divide by zero.")
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
-
+def request_sanitized_number(num):
+    try:
+        return float(num)
+    except ValueError:
+        while True:
+            try:
+                return float(input("Try again -- enter a float or an int: "))
+            except ValueError:
+                print("Wrong Answer")
+        
+        
 def main():
     
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = request_sanitized_number(input("Enter the first number: "))
+    num2 = request_sanitized_number(input("Enter the second number: "))
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
